15555e412fe1103cd6cd677e206affed78fd15f2
Lee Siong Chan <ahlee2326@me.com>
2019-05-07 23:27:36 +0800

    Update homepage

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -1,11 +1,85 @@
-import * as React from "react"
-import styled from "styled-components"
+import BackgroundImage from "gatsby-background-image"
+import Helmet from "react-helmet"
+import React from "react"
+import reset from "styled-reset"
+import styled, { createGlobalStyle } from "styled-components"
+import { graphql, useStaticQuery } from "gatsby"
 
-const MainLayout = styled.div`
-  width: 100%;
-  height: 100%;
-  background: teal;
-  min-height: 100vh;
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+
+  body {
+    color: #fff;
+  }
 `
 
-export default MainLayout
+const Grid = styled.div`
+  background-color: #0a0d15;
+  display: grid;
+  grid-template-areas: "left-panel right-panel";
+  grid-template-columns: 1fr 40%;
+  height: 100vh;
+`
+
+const LeftPanel = styled.div`
+  grid-area: left-panel;
+`
+
+const RightPanel = styled.div`
+  grid-area: right-panel;
+`
+
+const MainLayout = ({
+  children,
+  className,
+}: {
+  children?: React.ReactNode
+  className?: string
+}) => {
+  const data = useStaticQuery(graphql`
+    query {
+      desktop: file(relativePath: { eq: "bg.jpg" }) {
+        childImageSharp {
+          fluid(quality: 90, maxWidth: 4160) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+    }
+  `)
+  const backgroundImageData = data.desktop.childImageSharp.fluid
+
+  return (
+    <>
+      <GlobalStyle />
+      <Helmet>
+        <title>Octupus Technology</title>
+      </Helmet>
+
+      <Grid>
+        <LeftPanel>{children}</LeftPanel>
+
+        <RightPanel>
+          <BackgroundImage
+            className={className}
+            Tag="div"
+            fluid={backgroundImageData}
+            style={{
+              backgroundPosition: "center right",
+              backgroundSize: "contain",
+            }}
+          />
+        </RightPanel>
+      </Grid>
+    </>
+  )
+}
+
+const StyledMainLayout = styled(MainLayout)`
+  background-position: center right;
+  background-size: contain;
+  height: 100%;
+  width: 100%;
+`
+
+export default StyledMainLayout
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,49 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import React from "react"
+import TextLoop from "react-text-loop"
+import styled from "styled-components"
 
 import MainLayout from "../components/layouts/MainLayout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+
+const Container = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  justify-content: center;
+`
+
+const Title = styled.h1`
+  font-size: 24px;
+  margin-top: 50px;
+`
+
+const Description = styled.p`
+  font-size: 42px;
+  line-height: 50px;
+  max-width: 650px;
+
+  & b {
+    font-weight: bold;
+  }
+`
 
 const IndexPage = () => (
   <MainLayout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Container>
+      <div>
+        <Description>
+          3 hearts, 9 brains, just to make your{" "}
+          <TextLoop>
+            <b>mobile</b>
+            <b>tablet</b>
+            <b>website</b>
+            <b>system</b>
+          </TextLoop>{" "}
+          looks good.
+        </Description>
+        <Title>&mdash; Octupus Technology</Title>
+      </div>
+    </Container>
   </MainLayout>
 )
 

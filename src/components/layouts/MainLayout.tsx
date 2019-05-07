f3e55824704b631204e543b2182d5b4513c0f82d
Lee Siong Chan <ahlee2326@me.com>
2019-05-07 22:15:46 +0800

    Initial Commit

---
+++ src/components/layouts/MainLayout.tsx
@@ -0,0 +1,11 @@
+import * as React from "react"
+import styled from "styled-components"
+
+const MainLayout = styled.div`
+  width: 100%;
+  height: 100%;
+  background: teal;
+  min-height: 100vh;
+`
+
+export default MainLayout
